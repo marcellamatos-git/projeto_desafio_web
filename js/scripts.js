be33710e79d1c94607dec83ce7fe394aa9832065
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:35:32 -0300

    criado comandos de logica de progrmação em js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,90 @@
 console.log("teste")
+
+console.log("comando de saida")
+
+
+//comentario
+
+/*
+shdfsj
+sfdsf
+sfsf
+*/
+
+// variaveis
+
+let num = 50
+var num2 = 180
+const num3 = 360
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 450
+    var num5 = 150
+
+console.log(num4)
+}
+
+console.log(num5)
+num = 5
+console.log(num)
+num2 = 6
+console.log(num2)
+
+//num3 = 12 
+
+//concatenação
+console.log("valor de num " + num )
+console.log('valor de num3', num3)
+console.log(`valor de num2 ${num2}`)
+console.log('a soma dos numeros ' + (15 + 8))
+console.log("a soma dos numeros " , 15 + 8)
+console.log(`a soma dos numeros ${15 + 8 }` )
+console.log("a soma dos numeros " + num + num2)
+console.log(" a soma dos numeros " , num2 + 8)
+console.log(`a soma dos numeros ${num3 - num2}`)
+
+//operadores matematicos
+/*
++ soma
+- subtração
+* multiplicação
+/ divisão
+% modulo (resto da divisão)
+*/
+
+console.log("soma das variaveis " , num + num2)
+console.log("subtração das variaveis " , num3 - num)
+console.log("multiplicação " , num5 * 2)
+console.log(" divisão de valores ", num2/ 5)
+console.log("resto da divisão ", num2 % 2)
+console.log(`resto da divisao ${num3 % 3}`) 
+
+//operadores relacionais
+/*
+==igual
+>maior
+< menor
+<== menor igual
+!= diferente 
+ */
+
+//operadores logico
+/*
+&& E
+|| OU 
+*/
+ 
+console.log("teste de relacionais ", 10 == 5)
+console.log('teste relacionais ', 10 != 5)
+console.log('teste relacionais' , 10 > 5)
+console.log("teste relacionais " , 10 < 5)
+console.log('teste relacionais ' , 10 >= 5)
+console.log('teste relacionais ' , 10 <= 5)
+console.log("teste logivo" , (10 > 5) && (15 < 8))
+console.log("teste logivo" , (10 > 5) || (15 < 8))
+
+
