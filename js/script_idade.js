c66efabfaafdc8ccfabae4eb53995fd944a2115e
marcella <marcellamatos2428@gmail.com>
2026-06-12 15:04:46 -0300

    criado objFormDados

--- js/script_idade.js
+++ js/script_idade.js
@@ -8,4 +8,20 @@ const divResultado = document.querySelector("#div-dados")
 formDados.addEventListener('submit', (evt)=>{
     evt.preventDefault()
     
+
+    const objformDados = new FormData(formDados)
+    
+    let nome = objFormDados.get('nome')
+
+    let idade = objFormDados.get('idade')
+
+    let situacaoIdade = ''
+
+    if (idade >= 18){
+        situacaoIdade = `${nome}, voce é maior de idade`
+    }else{
+        situacaoIdade = `${nome}, vc é menor de idade`
+    }
+    divResultado.innerHTML = situacaoIdade
+
 })
