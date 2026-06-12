463d503fb68f8151a037890bdabc77f5cb64feef
marcella <marcellamatos2428@gmail.com>
2026-06-12 14:58:25 -0300

    submit formulario

--- js/script_idade.js
+++ js/script_idade.js
@@ -1,4 +1,11 @@
 //pegando elementos do dom
 
-const formDados  = document.querySelector('#formulariopessoa')
+const formDados  = document.querySelector('#formulario-pessoa')
 const divResultado = document.querySelector("#div-dados")
+
+//capturando o evento submit do formulário
+
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+    
+})
